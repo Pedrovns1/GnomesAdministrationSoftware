f0c3f766e721a6442043db888cdecaf16af62402
Pedro Vinicius <peuvini.94@gmail.com>
2024-02-24 19:35:45 +0000

    EmployeeTest class
    Here, I have created three Employee objects. These objects are then stored in an array named projectGroup. Then, I created a loop to iterate through each employee in the projectGroup array. The loop checks whether the employee's identification number condition represented by the variable m is true. If this condition is met, the program prints the name of the employee

--- src/gnomesadministrationsoftware/EmployeeTest.java
+++ src/gnomesadministrationsoftware/EmployeeTest.java
@@ -9,8 +9,26 @@ package gnomesadministrationsoftware;
  * @author peuvi
  */
 public class EmployeeTest {
-    
+
     public static void main(String[] args) {
+
+        // Employee objects with specified details
+        Employee emp1 = new Employee("Joe Bloggs", "jb@gmail.com");
+        Employee emp2 = new Employee("Ann Banana", "ab@gmail.com");
+        Employee emp3 = new Employee("Tom Thumb", "tt@gmail.com");
+
+        //Array created to store employees
+        Employee[] projectGroup = {emp1, emp2, emp3};
+
+        System.out.println("Next Employee number: " + Employee.getNextEmpNum());
+
+        // Loop to check and display the name of each employee loaded into the array
+        int m = 0;
+        for (Employee employee : projectGroup) {
+            if (employee.getempNum() > m) {
+                System.out.println("Employee name: " + employee.getName());
+            }
+        }
 
     }
 }
