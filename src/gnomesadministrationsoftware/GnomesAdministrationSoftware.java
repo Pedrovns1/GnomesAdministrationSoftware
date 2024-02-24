7c719e539c630e81349e41dc341a46c75950ecf1
Pedro Vinicius <peuvini.94@gmail.com>
2024-02-24 15:13:22 +0000

    Classes added
    I have created the classes that I will be using in my software here

---
+++ src/gnomesadministrationsoftware/GnomesAdministrationSoftware.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gnomesadministrationsoftware;
+
+/**
+ *
+ * @author peuvi
+ */
+public class GnomesAdministrationSoftware {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
