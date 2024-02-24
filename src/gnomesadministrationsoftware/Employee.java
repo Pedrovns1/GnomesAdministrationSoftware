67cb854bf58d11481f56d75af639c8a3f4aa851b
Pedro Vinicius <peuvini.94@gmail.com>
2024-02-24 18:50:55 +0000

    Getter & Set methods
    I made adjustments in the Employee class, fixing parameters and implementing getter methods. Additionally, I created a setEmail method to update the email when a valid input is provided. This method utilizes my isValidEmail validation to ensure the email is valid before updating

--- src/gnomesadministrationsoftware/Employee.java
+++ src/gnomesadministrationsoftware/Employee.java
@@ -13,14 +13,15 @@ public class Employee {
     private String name;
     private String email;
     private int empNum;
-    private static int nextempNum = 1;
+    private static int nextEmpNum = 1;
    
     // Default constructor 
     public Employee() {
+        // Will initialize with default values
         this.name = "Name";
         this.email = "Default@gnomes.com";
         //nextempNum++ will auto increment to ensure a unique employee
-        this.empNum = nextempNum++;
+        this.empNum = nextEmpNum++;
     }
     
     // Parametrized contructor with email validation
@@ -36,7 +37,37 @@ public class Employee {
             System.out.println("Please change this default email to a valid one");
             this.email = email;
         }
-        this.empNum = empNum;
+        // Initialize empNum with the next available employee number
+        this.empNum = nextEmpNum++;
     }
+  
+   // Name getter method
+   public String getName() {
+     return name;
+   }
+   
+   // Email getter method
+   public String getEmail() {
+     return email;
+   }
+    
+   // Employee number getter method
+   public int getempNum () {
+     return empNum;
+   }
 
+   // Email setter method using the regex pattern validation
+   public void setEmail (String email) {
+     if (EmailValidation.isValidEmail(email)) {
+       this.email = email;
+     }else {
+         // If the new email isn't valid the email will not be updated 
+         System.out.println("Invalid email. Email not updated");
+     }
+   }
+   
+   // Method to get the nex employee number available
+   public static int getNextEmpNum() {
+        return nextEmpNum;
+    }
 }
