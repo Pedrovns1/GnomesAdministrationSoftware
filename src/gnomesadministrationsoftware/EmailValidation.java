d302ac9300efd43d9f53ccdced3899df93ad9e84
Pedro Vinicius <peuvini.94@gmail.com>
2024-02-24 17:11:25 +0000

    Email Validation
    This code will validate the email using the regex pattern

--- src/gnomesadministrationsoftware/EmailValidation.java
+++ src/gnomesadministrationsoftware/EmailValidation.java
@@ -8,6 +8,17 @@ package gnomesadministrationsoftware;
  *
  * @author peuvi
  */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidation {
+    // Regex to check my email validation
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
     
+    // Validate the input email using the regex pattern
+    public static boolean isValidEmail(String email) {
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
+        return matcher.matches();
+    }
 }
