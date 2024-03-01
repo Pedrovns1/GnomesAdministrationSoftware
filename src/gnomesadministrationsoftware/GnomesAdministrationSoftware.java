e0d374e7f19b1cbd0443d1df453848796cf33458
Pedro Vinicius <peuvini.94@gmail.com>
2024-03-01 17:34:57 +0000

    Update Gnomes Administration Software
    Here, I have defined constants for the valid username and password that the manager uses to log in. Additionally, I implemented an isAuthenticated method to check whether the entered username and password match the correct ones. I have also started the implementation of a menu system.

--- src/gnomesadministrationsoftware/GnomesAdministrationSoftware.java
+++ src/gnomesadministrationsoftware/GnomesAdministrationSoftware.java
@@ -9,7 +9,25 @@ package gnomesadministrationsoftware;
  * @author peuvi
  */
 public class GnomesAdministrationSoftware {
-
     
+    // Constants for valid username and password to give access
+    private static final String USERNAME = "Gnomeo";
+    private static final String PASSWORD = "smurf";
+    
+    
+    // Method to handle the manager Login
+    private static boolean isAuthenticated(Manager manager, String enteredUsername, String enteredPassword) {
+        return manager.getUsername().equals(enteredUsername) && manager.getPassword().equals(enteredPassword);
+    }   
+    
+    // Method to display menu options for the manager
+    private static void displayMenu() {
+        System.out.println("Menu");
+        System.out.println("Enter your choice:");
+        System.out.println("1. View current staff");
+        System.out.println("2. Add new staff");
+        System.out.println("3. Remove staff");
+        System.out.println("4. Logout");
+    }
     
 }
