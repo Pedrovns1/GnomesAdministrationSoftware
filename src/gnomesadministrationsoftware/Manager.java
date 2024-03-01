34f69a53b038b34421253c1c514cdc9ba80cec65
Pedro Vinicius <peuvini.94@gmail.com>
2024-03-01 17:02:01 +0000

    Update Manager Class
    Here, I extended the functionality of my Employee class to my Manager class, defined the strings Username and password. I created a constructor for a Manager object with default values for the username and password. Additionally, two getter methods were implemented to get these values.

--- src/gnomesadministrationsoftware/Manager.java
+++ src/gnomesadministrationsoftware/Manager.java
@@ -8,6 +8,28 @@ package gnomesadministrationsoftware;
  *
  * @author peuvi
  */
-public class Manager {
-    
+
+// Manager class extends the functionality of the Employee class
+public class Manager extends Employee {
+
+    private String username;
+    private String password;
+
+    // Constructor for Manager with default values
+    public Manager() {
+        // Call the default constructor in the Employee class to ensure it initializes first
+        super(); 
+        this.username = "Gnomeo";
+        this.password = "smurf";
+    }
+
+    // Getter method for username
+    public String getUsername() {
+        return username;
+    }
+
+    // Getter method for password
+    public String getPassword() {
+        return password;
+    }
 }
